3c7a67ddb83a6a573032bc2065dcd8d3a2f6388f
root <root@vm-dyn-0-208.siame.univ-tlse3.fr>
2017-01-31 16:24:58 +0100

    ajout interruption sur le bouton user, et affichage sur l'UART

--- trace_generateur/stm32f4-discovery/src/main.c
+++ trace_generateur/stm32f4-discovery/src/main.c
@@ -1,22 +1,30 @@
 /**
- *  *    USB VCP for STM32F4xx example.
- *   *    
- *    *    @author     Tilen Majerle
- *     *    @email        tilen@majerle.eu
- *      *    @website    http://stm32f4-discovery.com
- *       *    @ide        Keil uVision
- *        *
- *         * Add line below to use this example with F429 Discovery board (in defines.h file)
- *          *
- *           * #define USE_USB_OTG_HS
- *            */
+ * USB VCP for STM32F4xx example.
+ *    
+ * @author     Tilen Majerle
+ * @email        tilen@majerle.eu
+ * @website    http://stm32f4-discovery.com
+ * @ide        Keil uVision
+ *
+ * Add line below to use this example with F429 Discovery board (in defines.h file)
+ *
+ * #define USE_USB_OTG_HS
+ **/
+#include "stm32f4xx.h"
+#include "stm32f4xx_exti.h"
+#include "stm32f4xx_syscfg.h"
+#include "misc.h"
+
 #include "tm_stm32f4_usb_vcp.h"
 #include "tm_stm32f4_disco.h"
 #include "defines.h"
  
+void Configure_PD0(void);
+void EXTI0_IRQHandler(void);
+
 int main(void) {
     uint8_t c;
-    /* System Init */
+    /* System Init -- UART */
     SystemInit();
     
     /* Initialize LED's. Make sure to check settings for your board in tm_stm32f4_disco.h file */
@@ -24,22 +32,94 @@ int main(void) {
     
     /* Initialize USB VCP */    
     TM_USB_VCP_Init();
+
+    /* Configure PD0 as interrupt */
+    Configure_PD0();
     
     while (1) {
-        /* USB configured OK, drivers OK */
+/*
+        // USB configured OK, drivers OK
         if (TM_USB_VCP_GetStatus() == TM_USB_VCP_CONNECTED) {
-            /* Turn on GREEN led */
+            // Turn on GREEN led
             TM_DISCO_LedOn(LED_GREEN);
             TM_DISCO_LedOff(LED_RED);
-            /* If something arrived at VCP */
+            // If something arrived at VCP
             if (TM_USB_VCP_Getc(&c) == TM_USB_VCP_DATA_OK) {
-                /* Return data back */
+                // Return data back
                 TM_USB_VCP_Putc('0');
             }
         } else {
-            /* USB not OK */
+            // USB not OK
             TM_DISCO_LedOff(LED_GREEN);
             TM_DISCO_LedOn(LED_RED);
         }
+*/
     }
+}
+
+/* Set interrupt handlers */
+/* Handle PD0 interrupt */
+void EXTI0_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+        /* Do your stuff when PD0 is changed */
+
+        if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0))
+            TM_USB_VCP_Puts("USER BUTTON : pushed\n\r");
+        else
+            TM_USB_VCP_Puts("USER BUTTON : released\n\r");
+        
+        
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}
+
+/* Configure pins to be interrupts */
+void Configure_PD0(void) {
+    /* Set variables used */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+    
+    /* Enable clock for GPIOD */
+    //RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    /* Enable clock for SYSCFG */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    
+    /* Set pin as input */
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
+    
+    /* Tell system that you will use PD0 for EXTI_Line0 */
+    //SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource0);
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+    
+    /* PD0 is connected to EXTI_Line0 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+ 
+    /* Add IRQ vector to NVIC */
+    /* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
 } 
